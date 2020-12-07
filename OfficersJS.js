a652204f80b4428a0bb950cd4afa6ec52650a8a2
EC2 Default User <ec2-user@ip-172-31-80-168.ec2.internal>
2020-12-07 22:43:32 +0000

    AJAX fixed, officers pg done

--- OfficersJS.js
+++ OfficersJS.js
@@ -2,21 +2,29 @@ $(function(){
     
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
-        console.log("Entered the xmlHttp onload function");
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
             console.log("Server ready");
             $(".container").empty();
-        }
-        else {
-            console.log(xmlHttp.readyState);
-            console.log(xmlHttp.status);
+            var xmlDoc = xmlHttp.responseXML;
+            console.dir(xmlDoc);
+            var officers = xmlDoc.getElementsByTagName("officer");
+            console.dir(officers);
+            var output;
+            for(var i = 0; i < officers.length; i++){
+                output += 
+                    "<div class='card clearfix'><img src="+
+                        officers[i].children[0].innerHTML+" alt="+
+                        officers[i].children[1].innerHTML+"><span class='PTitle words'>"+
+                        officers[i].children[2].innerHTML+"</span><p class='Description words'>"+
+                        officers[i].children[3].innerHTML+"</p></div>"
+            }
+            var container = document.getElementById('container');
+            container.innerHTML = output;
         }
     }
-/* Add the loader class */
+    /* Add the loader class */
     $(".container").append('<div class="loader"></div>');
     
-    xmlHttp.open("GET", "officerInfo/officerInfo.xml", true);
-    //console.log("Opened the xmlHttp connection");
-    xmlHttp.send;
-    //console.log("xmlHttp request sent");
+    xmlHttp.open("GET", "http://ec2-18-212-175-179.compute-1.amazonaws.com/CyberTigers/officerInfo/officerInfo.xml", true);
+    xmlHttp.send();
 });
