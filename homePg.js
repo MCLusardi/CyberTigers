e6a58f3fcc628804716f52d8b2559f8e458febb8
EC2 Default User <ec2-user@ip-172-31-80-168.ec2.internal>
2020-12-08 20:18:41 +0000

    added AJAX to slideshow and finished main page

--- homePg.js
+++ homePg.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+var slideIndex = 0;
 //call show slides for the first slide
 //showSlides(slideIndex);
 
@@ -37,3 +37,56 @@ function showSlides(n) {
     //mark dot n as "active"
     dots[slideIndex-1].className += " active";
 }
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var d = document.getElementsByClassName("dot");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+        d[i].className = d[i].className.replace(" active", "");
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    d[slideIndex-1].className += " active";
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
+$(function(){
+    
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            console.log("Server ready");
+            $(".container").empty();
+            var xmlDoc = xmlHttp.responseXML;
+            console.dir(xmlDoc);
+            var photos = xmlDoc.getElementsByTagName("photo");
+            console.dir(photos);
+            var PicOutput = "";
+            var DotOutput = "";
+            var counter = 1;
+            for(var i = 0; i < photos.length; i++){
+                PicOutput += 
+                    "<div class='mySlides fade'><div class='numbertext'>"+counter+"/"+photos.length+"</div><img src="+photos[i].children[0].innerHTML+" style='width:100%'><div class='text'>"+photos[i].children[1].innerHTML+"</div></div>";
+                DotOutput += "<span class='dot' onclick='currentSlide("+counter+")'></span>";
+                ++counter;
+            }
+            PicOutput += "<a class='prev' onclick='plusSlides(-1)'>&#10094;</a><a class='next' onclick='plusSlides(1)'>&#10095;</a>";
+            
+            console.log(PicOutput);
+            console.log(DotOutput);
+            var slideshow = document.getElementById('slideshow');
+            slideshow.innerHTML = PicOutput;
+            var dotCont = document.getElementById('dotContainer');
+            dotCont.innerHTML = DotOutput;
+            carousel();
+        }
+    }
+    /* Add the loader class */
+    $(".slideshow").append('<div class="loader"></div>');
+    
+    xmlHttp.open("GET", "http://ec2-18-212-175-179.compute-1.amazonaws.com/CyberTigers/openingSlideshow/photos.xml", true);
+    xmlHttp.send();
+});
